377a35d1f27f1643a5ef3023cfa6c68ac5d2591b
loren pabst <loren pabst>
2017-05-10 12:23:42 -0600

    added basic structure of server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+
+
+
+
+
+
+app.listen(8000);
---
+++ user.js
@@ -0,0 +1,54 @@
+var user = {
+  name: 'Loren',
+  location: 'Provo',
+  occupations: ['none'],
+  hobbies: [
+    {
+      name: 'reading',
+      type: 'lazy'
+    },
+    {
+      name: 'Skyrim',
+      type: 'video game'
+    },
+    {
+      name: 'hammocking',
+      type: 'lazy'
+    }
+  ],
+  family: [
+    {
+      name: 'Jessica',
+      relation: 'spouse',
+      gender: 'female'
+    },
+    {
+      name: 'Clayton',
+      relation: 'brother',
+      gender: 'male'
+    },{
+      name: 'Lucia',
+      relation: 'mom',
+      gender: 'female'
+    }
+  ],
+  restaurants: [
+    {
+      name: 'Texas Roadhouse',
+      type: 'American',
+      rating: 9.5
+    },
+    {
+      name: "Chili's",
+      type: 'American',
+      rating: 8.8
+    },
+    {
+      name: 'El Toro Viejo',
+      type: 'Mexican',
+      rating: 7.9
+    }
+  ]
+};
+
+module.exports = user;
